546d2ac92924073a5d37492f2b17cb934c30718b
trithangggggggg <trithang2006vn@gmail.com>
2024-12-12 00:52:25 +0700

    Add files via upload

---
+++ Ss14_Ex1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char chuoi[100];
+
+    printf("Nhap vao mot chuoi: ");
+    fgets(chuoi, sizeof(chuoi), stdin);
+
+    printf("Chuoi vua nhap: %s", chuoi);
+    printf("Do dai chuoi: %lu\n", strlen(chuoi) - 1);
+
+    return 0;
+}
+
